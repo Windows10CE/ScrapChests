e7eb375e0da729806e6b48192667eb0157bd41c9
Aaron Robinson <aaronrobin1234@gmail.com>
2020-11-26 20:36:25 -0600

    Move mod name, version, and GUID to consts

--- ScrapChestsPlugin.cs
+++ ScrapChestsPlugin.cs
@@ -5,13 +5,17 @@ using R2API.Utils;
 
 namespace ScrapChests
 {   
-    [BepInPlugin("com.Windows10CE.ScrapChests", "ScrapChests", "2.0.0")]
+    [BepInPlugin(ModGuid, ModName, ModVer)]
     [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
 
     [BepInDependency("com.bepis.r2api", BepInDependency.DependencyFlags.HardDependency)]
     [R2APISubmoduleDependency(new string[] { "ResourcesAPI", "LanguageAPI" })]
     public class ScrapChestsPlugin : BaseUnityPlugin
     {
+        private const string ModName = "ScrapChests";
+        private const string ModVer = "2.0.0";
+        private const string ModGuid = "com.Windows10CE.ScrapChests";
+
         internal static List<List<PickupIndex>> _cachedItemLists = new List<List<PickupIndex>>();
         internal static string[] _exceptionList = { "Duplicator", "LunarCauldron" };
 
