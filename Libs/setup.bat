3a18515a1298e23a2119d0fcec6b7f83ddf94029
Aaron Robinson <aaronrobin1234@gmail.com>
2020-12-09 17:36:51 -0600

    Edit library grabber script to only grab 1 commit

--- Libs/setup.bat
+++ Libs/setup.bat
@@ -1,4 +1,4 @@
-git clone https://github.com/harbingerofme/RoR2Libs.git
+git clone --depth 1 https://github.com/harbingerofme/RoR2Libs.git
 rmdir /S /Q RoR2Libs\.git
 copy RoR2Libs\*.dll .\
 copy RoR2Libs\*.xml .\
